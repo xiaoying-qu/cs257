8c090ace7efb6148d88005be16bc2f64f4cbdd90
Xiaoying Qu <testuser@Glahs-MacBook-Pro.local>
2022-10-12 00:37:48 -0500

    added queries.sql

--- olympics/queries.sql
+++ olympics/queries.sql
@@ -7,13 +7,15 @@ ORDER BY noc ASC;
 --List the names of all the athletes from Jamaica. If your database design allows it, sort the athletes by last name.
 SELECT athletes.athlete_name
 FROM athletes
-WHERE id in (SELECT DISTINCT athlete_id FROM event_results WHERE team_id in (SELECT id FROM teams WHERE team='Jamaica'))
+WHERE id in (SELECT DISTINCT athlete_id, athlete_ FROM event_results WHERE team_id in (SELECT id FROM teams WHERE team='Jamaica'))
 ORDER BY athlete_name ASC;
 
 
 --List all the medals won by Greg Louganis, sorted by year. Include whatever fields in this output that you think appropriate.
-
-
+SELECT medals.medal
+FROM medals
+WHERE medals.medal LIKE 'gold%' and medals.owner LIKE 'Greg Louganis'
+ORDER BY medals.game ASC;
 
 
 --List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
