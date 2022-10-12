da1570843f1a956cd24ee66173a00bc3358d4d85
Xiaoying Qu <testuser@Glahs-MacBook-Pro.local>
2022-10-11 21:55:39 -0500

    push new changes from convert.py

--- olympics/convert.py
+++ olympics/convert.py
@@ -17,35 +17,53 @@ with open('athlete_events.csv','r') as file,\
             athlete_id = row[0]
             athlete_name = row[1]
             athlete_sex = row[2]
+            athlete_age = "?" if 'NA' == row[3] else (row[3])
+            athlete_height = "?" if 'NA'== row[4] else (row[4])
+            athlete_weight = "?" if 'NA'== row[5] else (row[5])
             athlete_sport = row[12]
-            
+
             if athlete_id not in athletes:
                 athletes[athlete_id] = athlete_name
-                writer.writerow([athlete_id, athlete_name])
-            
+
+                writer.writerow([athlete_id, athlete_name, athlete_sex, athlete_age, athlete_height, \
+                    athlete_weight, athlete_sport])
+
+            medals = f"{row[8]}{row[13]} {row[14]}"
+            writer.writerow([medals])
+
+         
 # have a count variable
 
 olymic_games = {}
+olymic_games = set()
 with open('athlete_events.csv') as file,\
-        open('events.csv', 'w') as olymic_games_file:
+        open('olympic_games.csv', 'w') as olymic_games_file:
     reader = csv.reader(file)
     writer = csv.writer(olymic_games_file)
     # id, year, city, season
     next(reader)
     count = 1
     for row in reader:
+        game = row[8]
         game_year = row[9]
         game_season = row[10]
         game_city = row[11]
         
-        if game_year not in olymic_games and game_season not in olymic_games and game_city not in olymic_games:
+        if game not in olymic_games:
             game_id = count
-            writer.writerow([game_id, game_year, game_season, game_city])
+            olymic_games.add(game)
+            olymic_games.add(game_year)
+            olymic_games.add(game_season)
+            olymic_games.add(game_city)
+            writer.writerow([game_id, game, game_year, game_season, game_city])
             count+=1
+            
             
 
 
 events = {}
+# create an events set 
+events = set()
 with open('athlete_events.csv') as file,\
         open('events.csv', 'w') as events_file:
     reader = csv.reader(file)
@@ -53,12 +71,29 @@ with open('athlete_events.csv') as file,\
     next(reader)
     count = 1
     for row in reader:
-        event_name = row[13]
+        events_name = row[13]
+        events_name = events_name.replace(',','')
 
-        if event_name not in events:
-            event_id = count
-            writer.writerow([event_id, event_name])
+        if events_name not in events:
+            events_id = count
+            events.add(events_name)
+            writer.writerow([events_id, events_name])
             count+=1
 
-
-# a linking table named events_olympics_game so when you want to find out which athlete is at what game, the table will be helpful
+teams = {}
+teams = set()
+with open('athlete_events.csv') as file,\
+        open('teams.csv', 'w') as teams_file:
+    reader = csv.reader(file)
+    writer = csv.writer(teams_file)
+    next(reader)
+    count = 1
+    for row in reader:
+        team = row[6]
+        noc = row[7]
+        if team not in teams:
+            team_id = count
+            teams.add(team)
+            teams.add(noc)
+            writer.writerow([team_id, noc, team])
+            count+=1
